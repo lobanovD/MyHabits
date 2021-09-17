110853f59b980c7566726c83c9620d7bf9e1cd28
Dmitriy Lobanov <dvmald@icloud.com>
2021-09-17 15:05:40 +0300

    - добавил временный массив привычек

--- MyHabits/HabitsViewController.swift
+++ MyHabits/HabitsViewController.swift
@@ -9,21 +9,15 @@ import UIKit
 
 class HabitsViewController: UIViewController {
 
+    @IBOutlet weak var addNavigationItem: UIBarButtonItem!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        addNavigationItem.tintColor = ColorStyles.purple
 
-        // Do any additional setup after loading the view.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
 
 }
+
+
---
+++ MyHabits/TempHabits.swift
@@ -0,0 +1,18 @@
+//
+//  TempHabits.swift
+//  MyHabits
+//
+//  Created by Dmitrii Lobanov on 17.09.2021.
+//
+
+import Foundation
+
+let habbitsArray: [Habit] = [firstHabit, secondHabit, thridHabit, fourHabit, fifeHabit]
+
+let currentDateTime = Date()
+
+let firstHabit = Habit(name: "123", date: currentDateTime, trackDates: [currentDateTime], color: ColorStyles.purple)
+let secondHabit = Habit(name: "123", date: currentDateTime, trackDates: [currentDateTime], color: ColorStyles.purple)
+let thridHabit = Habit(name: "123", date: currentDateTime, trackDates: [currentDateTime], color: ColorStyles.purple)
+let fourHabit = Habit(name: "123", date: currentDateTime, trackDates: [currentDateTime], color: ColorStyles.purple)
+let fifeHabit = Habit(name: "123", date: currentDateTime, trackDates: [currentDateTime], color: ColorStyles.purple)
